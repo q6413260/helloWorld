ef1dde430d2d48428c75d117c0b3c9f1f157aa79
yeming <yeming@meituan.com>
2019-01-30 13:57:06 +0800

    回溯 正则表达式

--- src/main/java/algo/Backtracking.java
+++ src/main/java/algo/Backtracking.java
@@ -1,29 +1,51 @@
 package algo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by xiaoming on 29/01/2019.
  */
 public class Backtracking {
     private int maxW = Integer.MIN_VALUE; // 存储背包中物品总重量的最大值
+    // 下标表示物品序号，值表示是否放进背包:1放，0不放
+    private int[] currentAnswer;
+    //存储所有解(map key表示放进去的重量，value表示对应重量的物品放法)，
+    //最终所有最优解为bestAnswerMap.get(maxW)
+    private Map<Integer, List<int[]>> bestAnswerMap = new HashMap();
+
     // cw 表示当前已经装进去的物品的重量和；i 表示考察到哪个物品了；
     // w 背包重量；items 表示每个物品的重量；n 表示物品个数
     // 假设背包可承受重量 100，物品个数 10，物品重量存储在数组 a 中，那可以这样调用函数：
     // f(0, 0, a, 10, 100)
-    private int[] bestAnswer;// 解
-
     public void f(int i, int cw, int[] items, int n, int w) {
-        if(bestAnswer == null){
-            bestAnswer = new int[n];
+        if(currentAnswer == null){
+            currentAnswer = new int[n];
         }
 
         if (cw == w || i == n) { // cw==w 表示装满了 ;i==n 表示已经考察完所有的物品
-            if (cw > maxW) maxW = cw;
+            if (cw >= maxW) {
+                maxW = cw;
+                int[] bestAnswer = new int[currentAnswer.length];
+                for(int j=0; j<currentAnswer.length; j++){
+                    bestAnswer[j] = currentAnswer[j];
+                }
+                if(bestAnswerMap.containsKey(cw)){
+                    bestAnswerMap.get(cw).add(bestAnswer);
+                }else{
+                    List<int[]> list = new ArrayList<int[]>();
+                    list.add(bestAnswer);
+                    bestAnswerMap.put(cw, list);
+                }
+            }
             return;
         }
-        bestAnswer[i] = 0;
+        currentAnswer[i] = 0;
         f(i+1, cw, items, n, w);
         if (cw + items[i] <= w) {// 已经超过可以背包承受的重量的时候，就不要再装了
-            bestAnswer[i] = 1;
+            currentAnswer[i] = 1;
             f(i+1,cw + items[i], items, n, w);
         }
     }
@@ -90,18 +112,16 @@ public class Backtracking {
         Backtracking backtracking = new Backtracking();
         int[] items = new int[]{20,10,5,8,9,31,30,13,16,19};
         backtracking.f(0, 0, items, 10, 100);
-        System.out.println(backtracking.bestAnswer);
-        int sum = 0;
-        for(int i=0; i<backtracking.bestAnswer.length; i++){
-            if(backtracking.bestAnswer[i] == 1){
-                sum += items[i];
-                System.out.println("装进去的物品序号:" + i);
-            }
-        }
+//        System.out.println(backtracking.bestAnswer);
+//        int sum = 0;
+//        for(int i=0; i<backtracking.bestAnswer.length; i++){
+//            if(backtracking.bestAnswer[i] == 1){
+//                sum += items[i];
+//                System.out.println("装进去的物品序号:" + i);
+//            }
+//        }
 
-        System.out.println("总重量:" + sum);
-
-        backtracking.cal8queens(0);
+//        backtracking.cal8queens(0);
     }
 }
 
