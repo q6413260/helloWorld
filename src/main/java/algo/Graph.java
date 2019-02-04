58decaa37b4dd342dc00675e3fb00514ea7e5575
yeming <228160255@qq.com>
2019-02-04 17:38:19 +0800

    利用kahn算法和dfs算法进行拓扑排序

--- src/main/java/algo/Graph.java
+++ src/main/java/algo/Graph.java
@@ -3,7 +3,7 @@ package algo;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**无向图bfs和dfs搜索
+/**无向图bfs和dfs搜索；利用图进行kahn排序和dfs
  * Created by xiaoming on 24/01/2019.
  */
 public class Graph {
@@ -23,6 +23,76 @@ public class Graph {
     public void addEdge(int s, int t){
         array[s].add(t);
         array[t].add(s);
+    }
+
+    public void addSingleEdge(int s, int t){
+        array[s].add(t);
+    }
+
+    public void topoSortByKahn(){
+        int[] indegree = new int[v];
+        //计算所有顶点的入度
+        for(int i=0; i< array.length; i++){
+            for(int j=0; j<array[i].size(); j++){
+                indegree[array[i].get(j)] ++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for(int i=0; i<v; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()){
+            int v = queue.poll();
+
+            System.out.println("输出:" + v);
+            for(int i=0; i<array[v].size(); i++){
+                int next = array[v].get(i);
+
+                if(--indegree[next] == 0){
+                    queue.add(next);
+                }
+            }
+        }
+    }
+
+    public void topoSortByDfs(){
+        //构建逆邻接表
+        LinkedList<Integer>[] inverseAdj = new LinkedList[v];
+        for(int i=0; i<v; i++){
+            inverseAdj[i] = new LinkedList<Integer>();
+        }
+        for(int i=0; i<v; i++){
+            for(int j=0; j<array[i].size(); j++){
+                int next = array[i].get(j);
+                inverseAdj[next].add(i);
+            }
+        }
+        boolean[] visited = new boolean[v];
+        for(int i=0; i<v; i++){
+            if(visited[i]){
+                continue;
+            }
+
+            dfs(i, inverseAdj, visited);
+        }
+    }
+
+    private void dfs(int v, LinkedList<Integer>[] inverseAdj, boolean[] visited){
+        visited[v] = true;
+        for(int i=0; i<inverseAdj[v].size(); i++){
+            int next = inverseAdj[v].get(i);
+
+            if(!visited[next]){
+                dfs(next, inverseAdj, visited);
+            }
+        }
+
+        // 先把 vertex 这个顶点可达的所有顶点都打印出来之...
+        System.out.println("输出:" + v);
     }
 
     public void bfs(int s, int t){
@@ -139,18 +209,25 @@ public class Graph {
     }
 
     public static void main(String[] args) {
-        Graph graph = new Graph(8);
-        graph.addEdge(0,1);
-        graph.addEdge(0,3);
-        graph.addEdge(1,2);
-        graph.addEdge(1,4);
-        graph.addEdge(2,5);
-        graph.addEdge(3,4);
-        graph.addEdge(4,5);
-        graph.addEdge(4,6);
-        graph.addEdge(5,7);
-        graph.addEdge(6,7);
-
-        graph.dfs(5, 7);
+//        Graph graph = new Graph(8);
+//        graph.addEdge(0,1);
+//        graph.addEdge(0,3);
+//        graph.addEdge(1,2);
+//        graph.addEdge(1,4);
+//        graph.addEdge(2,5);
+//        graph.addEdge(3,4);
+//        graph.addEdge(4,5);
+//        graph.addEdge(4,6);
+//        graph.addEdge(5,7);
+//        graph.addEdge(6,7);
+//        graph.dfs(5, 7);
+        Graph graph = new Graph(5);
+        graph.addSingleEdge(0, 1);
+        graph.addSingleEdge(0, 3);
+        graph.addSingleEdge(1, 2);
+        graph.addSingleEdge(2, 3);
+        graph.addSingleEdge(2, 4);
+        graph.addSingleEdge(4, 3);
+        graph.topoSortByDfs();
     }
 }
